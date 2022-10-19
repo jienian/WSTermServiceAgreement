206d308a936619e8b591c821e4cf9fd705180be1
jiangjienian <2129300193@qq.com>
2022-10-20 02:05:13 +0800

    register界面

--- pages/register/register.js
+++ pages/register/register.js
@@ -5,9 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    checkValue:1,
+    check:false,
+  
+    sex:[{
+      id:1,
+      value:'男'
+    },{
+      id:2,
+      value:'女'
+    }
+  ]
   },
-
+  radioChange:function(e){
+const sex=this.data.sex
+for (let i = 0, len = sex.length; i < len; ++i) {
+  sex[i].checked = sex[i].id == e.detail.value
+}
+this.setData({
+  sex
+})
+console.log(this.data.sex);
+},
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -62,5 +82,21 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
+  bindDateChange(e) {
+    this.setData({
+      date: e.detail.value,
+    });
+  },
+  checkboxChange(e) {
+    if (e.detail.value.includes('1')) {
+      this.setData({
+        check: true,
+      });
+    } else {
+      this.setData({
+        check: false,
+      });
+    }
+  },
 })
